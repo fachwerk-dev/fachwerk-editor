0a3bd780ac24aae187efa58af7643040f9cd77e8
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-04-05 15:28:46 +0300

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { ref, watchEffect } from "vue";
+import { useStorage } from "@vueuse/core";
+
 import {
   utoa,
   atou,
   Content,
   Compiler,
   compileMarkdown,
-  isScriptSetup,
 } from "fachwerk/internal";
 
 import Monaco from "./components/Monaco.vue";
-import Debug from "./components/Debug.vue";
 
 const content = ref(atou(location.hash.slice(1)));
 
@@ -19,50 +19,21 @@ watchEffect(() => {
 });
 
 const onError = (e: any) => console.log(e);
+
+const showPreview = useStorage("fachwerk_preview", false);
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2">
-    <Monaco class="h-[80vh] md:h-screen" v-model="content" />
-    <Content class="p-4" :class="{ 'h-screen w-full': isScriptSetup(content) }">
+    <Monaco class="h-screen" v-model="content" v-if="!showPreview" />
+    <Content class="p-4 md:p-6">
       <Compiler :source="compileMarkdown(content)" @error="onError" />
     </Content>
   </div>
-  <Debug />
+  <div
+    class="block md:hidden bg-gray-700 px-3 py-1 rounded text-sm text-gray-200 fixed bottom-3 left-3"
+    @click="showPreview = !showPreview"
+  >
+    {{ showPreview ? "Code" : "Preview" }}
+  </div>
 </template>
-
-<style>
-/*
-:root {
-  --sans-serif: "IBM Plex Sans", sans-serif;
-  --mono: Cousine, monospace;
-}
-body {
-  margin: 0;
-  font-family: var(--sans-serif);
-  color: #2c3e50;
-}
-pre,
-code {
-  font-family: var(--mono);
-  font-feature-settings: "liga" 0, "calt" 0;
-}
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  margin-top: 0;
-  font-weight: 600;
-}
-p {
-  margin: 0 0 1rem 0;
-  line-height: 1.7;
-}
-svg,
-input {
-  display: block;
-}
-*/
-</style>
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { Fachwerk } from "fachwerk";
+
+import "fachwerk/internal.css";
 import "./app.css";
 
 const app = createApp(App);
